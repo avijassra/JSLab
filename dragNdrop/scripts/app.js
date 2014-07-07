1fb24eef073969995804665d4b39e506c9ace39e
Avi Jassra <avijassra@hotmail.com>
2014-07-07 13:14:09 -0400

    test sorting

--- dragNdrop/scripts/app.js
+++ dragNdrop/scripts/app.js
@@ -1,6 +1,7 @@
-var app = angular.module('dragNdropApp', ['ui']);
+var app = angular.module('dragNdropApp', []);
 
-app.factory('lodashSrvc', function() {
+app.factory('lodashSrvc', function () {
+    //'use strict'
     return _;
 });
 
@@ -91,6 +92,44 @@ app.directive('droppable', function() {
 	}
 });
 
+// directive for a single list
+app.directive('dndList', function () {
+    return function (scope, element, attrs) {
+        debugger;
+        // variables used for dnd
+        var toUpdate;
+        var startIndex = -1;
+        // watch the model, so we always know what element
+        // is at a specific position
+        scope.$watch(attrs.dndList, function (value) {
+            toUpdate = value;
+        }, true);
+        // use jquery to make the element sortable (dnd). This is called
+        // when the element is rendered
+        $(element[0]).sortable({
+            items: 'div.dgble',
+            //items: 'div.masterFeeLineItemGridStyle div.ng-scope.ngRow',
+            start: function (event, ui) {
+                // on start we define where the item is dragged from
+                startIndex = ($(ui.item).index());
+            },
+            stop: function (event, ui) {
+                // on stop we determine the new index of the
+                // item and store it there
+                var newIndex = ($(ui.item).index());
+                var toMove = toUpdate[startIndex];
+                toUpdate.splice(startIndex, 1);
+                toUpdate.splice(newIndex, 0, toMove);
+                // we move items in the array, if we want
+                // to trigger an update in angular use $apply()
+                // since we're outside angulars lifecycle
+                scope.$apply(scope.model);
+            },
+            axis: 'y'
+        })
+    }
+});
+
 app.controller('MainCtrl', function($scope, getDataSrvc, lodashSrvc) {
     $scope.users = getDataSrvc.users;
     $scope.selectedUsers = [];
@@ -98,7 +137,7 @@ app.controller('MainCtrl', function($scope, getDataSrvc, lodashSrvc) {
   $scope.handleDrop = function(id) {
       selectedUser = lodashSrvc.filter($scope.users, {'id': parseInt(id)})[0];
       debugger;
-      if(!selectedUser.selected) {
+      if(selectedUser && !selectedUser.selected) {
         selectedUser.selected = true;
         $scope.selectedUsers.push(selectedUser);
       }
