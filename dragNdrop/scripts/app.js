af539b31181ffaa0e6a7a6a24ac71d2ee9d00aa5
Avi Jassra <avijassra@hotmail.com>
2014-07-11 17:25:22 -0400

    Revert "working drag and drop"
    This reverts commit 11f56c4d34214d2225bfc5f8e9462f5798608110.

--- dragNdrop/scripts/app.js
+++ dragNdrop/scripts/app.js
@@ -32,7 +32,6 @@ app.directive('droppable', function() {
 			var el = element[0];
 
 			el.addEventListener('dragover', function(e) {
-				debugger;
 				e.dataTransfer.dropEffect = 'move';
 				// allows us to drop
 				if (e.preventDefault) e.preventDefault();
@@ -41,19 +40,16 @@ app.directive('droppable', function() {
 			}, false);
 
 			el.addEventListener('dragenter', function(e) {
-				debugger;
 				this.classList.add('over');
 				return false;
 			}, false);
 
 			el.addEventListener('dragleave', function(e) {
-				debugger;
 				this.classList.remove('over');
 				return false;
 			}, false);
 
 			el.addEventListener('drop', function(e) {
-				debugger;
 				// Stops some browsers from redirecting.
 				if (e.stopPropagation) e.stopPropagation();
 
@@ -78,6 +74,6 @@ app.directive('droppable', function() {
 
 app.controller('MainCtrl', function($scope) {
   $scope.handleDrop = function(item, bin) {
-    //alert('Item ' + item + ' has been dropped into ' + bin);
+    alert('Item ' + item + ' has been dropped into ' + bin);
   }
 });
