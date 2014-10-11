46611e851b3e047781b82403c1eac42e1491c619
Avi Jassra <avijassra@hotmail.com>
2014-10-11 14:13:17 -0400

    added new button

--- ngTodo/app/list/controller.js
+++ ngTodo/app/list/controller.js
@@ -3,6 +3,18 @@
         .controller('TodoCtrl', ['$scope', 'todoRepo', _todoCtrl]);
     
     function _todoCtrl($scope, todoRepo) {
+        $scope.todos = []
+        $scope.addNewTask = addNewTaskButtonClickHandler;
+        
+        function addNewTaskButtonClickHandler() {
+            var newId = $scope.todos.length + 1
+            
+            $scope.todos.push({
+                id: newId,
+                task: "New task" + newId,
+                isComplete: false
+            });
+        }
         
         initView();
         
